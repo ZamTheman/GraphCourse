d95470c0128b4c0b4a6c7852ce24f51db7e75775
samuel <samuel.wahlberget@gmail.com>
2021-12-26 19:04:50 +0100

    Updated dfs with undirected cycle detection

--- Lectures/dfs.cpp
+++ Lectures/dfs.cpp
@@ -54,24 +54,53 @@ public:
     }
 };
 
-int main(){
+bool solve(int n, vector<vector<int>> edges) {
     Graph g;
-    g.addEdge(0,1);
-    g.addEdge(1,2);
-    g.addEdge(2,3);
-    g.addEdge(3,5);
-    g.addEdge(5,6);
-    g.addEdge(4,5);
-    g.addEdge(0,4);
-    g.addEdge(3,4);
-    // g.dfs(1);
-    cout << g.dfs_cycle_detection(1, -1) << endl;
+    for (auto edge : edges)
+        g.addEdge(edge[0], edge[1]);
+        
+    return g.dfs_cycle_detection(edges[0][0], -1);
+}
 
-    Graph g1;
-    g1.addEdge(0,1);
-    g1.addEdge(1,2);
-    g1.addEdge(2,3);
-    cout << g1.dfs_cycle_detection(0, -1) << endl;
+int main(){
+    // Graph g;
+    // g.addEdge(0,1);
+    // g.addEdge(1,2);
+    // g.addEdge(2,3);
+    // g.addEdge(3,5);
+    // g.addEdge(5,6);
+    // g.addEdge(4,5);
+    // g.addEdge(0,4);
+    // g.addEdge(3,4);
+    // // g.dfs(1);
+    // cout << g.dfs_cycle_detection(1, -1) << endl;
+
+    // Graph g1;
+    // g1.addEdge(0,1);
+    // g1.addEdge(1,2);
+    // g1.addEdge(2,3);
+    // cout << g1.dfs_cycle_detection(0, -1) << endl;
+
+    int n = 5;
+    vector<vector<int>> edges {
+        { 1, 2 },
+        { 1, 3 },
+        { 2, 3 },
+        { 1, 4 },
+        { 4, 5 }
+    };
+
+    vector<vector<int>> edges2 {
+        { 3, 5 },
+        { 10, 3 },
+        { 4, 10 },
+        { 4, 2 },
+        { 4, 7 },
+        { 10, 5 },
+        { 4, 6 }
+    };
+    
+    cout << solve(n,edges2) << endl;
 
     return 0;
 }
